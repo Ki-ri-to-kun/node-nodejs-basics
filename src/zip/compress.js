5e61f0f3496e236ca015b47a8c654be0758c8e94
stas <staskazakov9@gmail.com>
2024-10-01 16:21:17 +0600

    fix: zlib done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import zlib from 'zlib';
+import fs from 'fs';
+  
 const compress = async () => {
-    // Write your code here 
+   const input = fs.createReadStream('files/fileToCompress.txt');
+   const output = fs.createWriteStream('files/archive.gz');
+   
+   const compress = zlib.createGzip();
+   
+   input.pipe(compress).pipe(output);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import zlib from 'zlib';
+import fs from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+   const input = fs.createReadStream('files/archive.gz');
+   const output = fs.createWriteStream('files/fileToCompress.txt');
+   
+   const decompress = zlib.createUnzip();
+   
+   input.pipe(decompress).pipe(output);
 };
 
 await decompress();
