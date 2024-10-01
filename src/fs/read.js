46a8ac671e395146a05a475f2be006bac02afd73
stas <staskazakov9@gmail.com>
2024-10-01 21:43:26 +0600

    feat: read file done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const conditionFail = !fs.existsSync('files/fileToRead.txt');
+    
+    if(conditionFail){
+       throw new Error('FS operation failed');
+    } else {
+      const readStream = fs.createReadStream('files/fileToRead.txt');
+      readStream.on('data', (data) => console.log(data.toString()));
+    }
 };
 
 await read();
