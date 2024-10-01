0d2b2e01a49b082cd248ed9bf25791eddc0df78c
stas <staskazakov9@gmail.com>
2024-10-01 18:46:09 +0600

    feat: rename file done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    const conditionFail = !fs.existsSync('files/wrongFilename.txt') || fs.existsSync('files/properFilename.md');
+    
+    if(conditionFail){
+      throw new Error('FS operation failed');
+    } else {
+      fs.rename('files/wrongFilename.txt', 'files/properFilename.md', (err) => err && console.log(err));
+    }
+    
 };
 
 await rename();
