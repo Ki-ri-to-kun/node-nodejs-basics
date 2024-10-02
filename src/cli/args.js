1ae63e486760c5b1e9dda137aedf3eb8e98bcbbb
stas <staskazakov9@gmail.com>
2024-10-02 10:28:05 +0600

    feat: args done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+   let strResult = '';
+  
+   process.argv.forEach((val, index) => {
+     if(index < 2) return;
+     
+     if(index % 2 === 0){
+      strResult += `${val.substring(2)} is `;
+     } else {
+      strResult += val;
+      console.log(strResult);
+      strResult = '';
+     }
+   });
+   
 };
 
 parseArgs();
