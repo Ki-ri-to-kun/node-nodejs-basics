a49a4a032d7a4513380f644a459a17788a6c2801
stas <staskazakov9@gmail.com>
2024-10-05 00:05:26 +0600

    feat: worker_threads done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import os from 'os';
+import {Worker, workerData} from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const coresNumber = os.cpus();
+    const actualCoresNumber = Object.entries(coresNumber).length;
+    
+    const promiseArray = [];
+    const resultArray = [];
+    
+    for(let i = 10; i < actualCoresNumber + 10; i++){ 
+      const resultPromise = new Promise((resolve, reject) => {
+        const worker = new Worker('./worker.js', {workerData: i});
+        worker.on('message', data => resolve(resultArray.push({id: worker.threadId, status: 'resolved', data})));
+        worker.on('error', error => reject(resultArray.push({id: worker.threadId, status: 'error', data: null})));
+    });
+      promiseArray.push(resultPromise);
+    }
+   
+   Promise.allSettled(promiseArray).then(data => {
+     resultArray.sort((a, b) => {
+       if(a.id < b.id) return -1;
+       if(a.id > b.id) return 1;
+     });
+     
+       for(let item of resultArray){
+          delete item.id;
+          console.log(item);
+       }
+   }); 
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
+import {parentPort, workerData} from 'node:worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    const result = nthFibonacci(workerData);
+    
+    // error test 
+    //if(workerData === 11)throw new Error('error!');
+    
+    parentPort.postMessage(result);
 };
 
 sendResult();
