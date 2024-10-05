1b3de6f0309abfefe015c55cf2fba917d2674625
stas <staskazakov9@gmail.com>
2024-10-05 16:25:44 +0600

    feat:child_processes done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import {fork} from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const argsToSend = args ? [...args] : [];
+  const childProcess = fork('files/script.js', argsToSend);
+    
+  childProcess.on('message', data => process.stdout.write(data));
+  childProcess.on('error', () => process.exit());
+  process.stdin.on('data', data => childProcess.send(data.toString()));
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
